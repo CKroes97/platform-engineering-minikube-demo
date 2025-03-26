8e4f8cb63ddb3ba58d4868a676648772fe30f495
CasperK97 <awesomium@hotmail.nl>
2025-03-26 20:45:33 +0100

    clean up

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
+
 usage() {
     echo "Usage: setup.sh --github-runner-token <token> [options]"
     echo "Required:"
     echo "  --github-runner-token <token>    GitHub Actions runner token"
     exit 1
 }
-
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
@@ -42,7 +43,7 @@ sudo ./svc.sh start
 
 cd ~
 
-# install terraform
+# install terraform repo
 sudo apt install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
@@ -53,13 +54,18 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update
+cd ~
 
-sudo apt install terraform
+# install kubectl repo
+sudo apt install -y apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 
 cd ~
 
-# install docker
+# install docker repo
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -70,21 +76,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# install packages
+sudo apt install -y kubectl terraform docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 
-sudo usermod -aG docker $USER && newgrp docker
-
-cd ~
-
-# install kubectl
-
-sudo apt install -y apt-transport-https ca-certificates curl gnupg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt install -y kubectl
+sudo usermod -aG docker $USER
 
 cd ~
 
@@ -92,5 +87,6 @@ cd ~
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 minikube start --kubernetes-version=v1.32.0
+
 
 
