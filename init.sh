53f4eaf85377daf367e559cc3a67d842ed24c7bf
CKroes97 <43654838+CKroes97@users.noreply.github.com>
2025-03-27 18:41:04 +0100

    Initial commit (#1)
    Basic functionality to spin up a minikube environment using Ansible and the being able to deploy to it using terraform.

---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+    echo "Usage: setup.sh --github-runner-token <token> [options]"
+    echo "Required:"
+    echo "  --github-runner-token <token>    GitHub Actions runner token"
+    exit 1
+}
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --github-runner-token) GITHUB_RUNNER_TOKEN="$2"; shift ;;
+        -h|--help) usage ;;
+        *) echo "Unknown parameter: $1"; usage ;;
+    esac
+    shift
+done
+
+if [[ -z "$GITHUB_RUNNER_TOKEN" ]]; then
+    echo "Error: --github-runner-token is required."
+    usage
+fi
+
+cd ~
+
+# install github runner
+mkdir actions-runner && cd actions-runner
+
+curl -o actions-runner-linux-x64-2.323.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
+
+echo "0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" | shasum -a 256 -c
+
+tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz
+
+./config.sh --url https://github.com/CKroes97/platform-engineering-minikube-demo --token $GITHUB_RUNNER_TOKEN
+
+sudo ./svc.sh install
+
+sudo ./svc.sh start
---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+  backend "local" {
+   path = "/var/lib/plateng/terraform.tfstate"
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "registry_namespace" {
+  metadata {
+    name = "docker-registry"
+  }
+}
