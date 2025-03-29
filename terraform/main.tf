e81dae24e92ecf33e3daea701aab22481519f471
CasperK97 <awesomium@hotmail.nl>
2025-03-29 17:06:53 +0100

    improve reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace" "registry_namespace" {
 
 resource "helm_release" "harbor" {
   name       = "harbor"
-  namespace  = registry_namespace.name
+  namespace  = kubernetes_namespace.registry_namespace.metadata.name
   repository = "https://helm.goharbor.io"
   chart      = "harbor"
   version    = "2.9.0"  # Specify the desired version
