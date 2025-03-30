97c7fc7cc310daefa3042358f1f9e4c59a05a3f4
CasperK97 <awesomium@hotmail.nl>
2025-03-30 16:10:16 +0200

    add harbor cli and passwords

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,73 @@ resource "helm_release" "harbor" {
   namespace = kubernetes_namespace.registry_namespace.metadata[0].name
   repository= "https://helm.goharbor.io"
   chart     = "harbor"
-  values    = [file("helm_values/harbor.yaml")]
+  values = [templatefile("${path.module}/values.yaml", {
+    HARBOR_URL      = var.harbor_url
+    HARBOR_USERNAME = var.harbor_username
+    HARBOR_PASSWORD = var.harbor_password
+  })]
 }
 
+resource "kubernetes_secret" "harbor_credentials" {
+  metadata {
+    name      = "harbor-credentials"
+    namespace = "default"
+  }
+
+  data = {
+    HARBOR_URL      = var.harbor_url
+    HARBOR_USERNAME = var.harbor_username
+    HARBOR_PASSWORD = var.harbor_password
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_deployment" "harbor_cli" {
+  metadata {
+    name      = "harbor-cli"
+    namespace = "default"
+    labels = {
+      app = "harbor-cli"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "harbor-cli"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "harbor-cli"
+        }
+      }
+
+      spec {
+        container {
+          name  = "hcli"
+          image = "goharbor/harbor-cli:latest"
+
+          command = ["/bin/sh", "-c", "sleep infinity"]
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.harbor_credentials.metadata[0].name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+# Terraform Variables for GitHub Secrets
+variable "harbor_url" {}
+variable "harbor_username" {}
+variable "harbor_password" {}
