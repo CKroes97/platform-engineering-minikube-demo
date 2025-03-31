906a5dc0616e9cec1fb47f81e33e55bc4f482a5a
CasperK97 <awesomium@hotmail.nl>
2025-03-31 19:07:21 +0200

    end debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,88 +1,78 @@
-locals {
-  debug_harbor_url      = var.harbor_url
-  debug_harbor_username = var.harbor_username
-  debug_harbor_password = var.harbor_password
+resource "kubernetes_namespace" "registry_namespace" {
+  metadata {
+    name = "docker-registry"
+  }
 }
 
-output "debug_harbor_url" {
-  value = local.debug_harbor_url
+resource "helm_release" "harbor" {
+  name      = "harbor"
+  namespace = kubernetes_namespace.registry_namespace.metadata[0].name
+  repository= "https://helm.goharbor.io"
+  chart     = "harbor"
+  values = [templatefile("${path.module}/values.yaml", {
+    HARBOR_URL      = var.harbor_url
+    HARBOR_USERNAME = var.harbor_username
+    HARBOR_PASSWORD = var.harbor_password
+  })]
 }
 
-# resource "kubernetes_namespace" "registry_namespace" {
-#   metadata {
-#     name = "docker-registry"
-#   }
-# }
+resource "kubernetes_secret" "harbor_credentials" {
+  metadata {
+    name      = "harbor-credentials"
+    namespace = "default"
+  }
 
-# resource "helm_release" "harbor" {
-#   name      = "harbor"
-#   namespace = kubernetes_namespace.registry_namespace.metadata[0].name
-#   repository= "https://helm.goharbor.io"
-#   chart     = "harbor"
-#   values = [templatefile("${path.module}/values.yaml", {
-#     HARBOR_URL      = var.harbor_url
-#     HARBOR_USERNAME = var.harbor_username
-#     HARBOR_PASSWORD = var.harbor_password
-#   })]
-# }
+  data = {
+    HARBOR_URL      = var.harbor_url
+    HARBOR_USERNAME = var.harbor_username
+    HARBOR_PASSWORD = var.harbor_password
+  }
 
-# resource "kubernetes_secret" "harbor_credentials" {
-#   metadata {
-#     name      = "harbor-credentials"
-#     namespace = "default"
-#   }
+  type = "Opaque"
+}
 
-#   data = {
-#     HARBOR_URL      = var.harbor_url
-#     HARBOR_USERNAME = var.harbor_username
-#     HARBOR_PASSWORD = var.harbor_password
-#   }
+resource "kubernetes_deployment" "harbor_cli" {
+  metadata {
+    name      = "harbor-cli"
+    namespace = "default"
+    labels = {
+      app = "harbor-cli"
+    }
+  }
 
-#   type = "Opaque"
-# }
+  spec {
+    replicas = 1
 
-# resource "kubernetes_deployment" "harbor_cli" {
-#   metadata {
-#     name      = "harbor-cli"
-#     namespace = "default"
-#     labels = {
-#       app = "harbor-cli"
-#     }
-#   }
+    selector {
+      match_labels = {
+        app = "harbor-cli"
+      }
+    }
 
-#   spec {
-#     replicas = 1
+    template {
+      metadata {
+        labels = {
+          app = "harbor-cli"
+        }
+      }
 
-#     selector {
-#       match_labels = {
-#         app = "harbor-cli"
-#       }
-#     }
+      spec {
+        container {
+          name  = "hcli"
+          image = "goharbor/harbor-cli:latest"
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = "harbor-cli"
-#         }
-#       }
+          command = ["/bin/sh", "-c", "sleep infinity"]
 
-#       spec {
-#         container {
-#           name  = "hcli"
-#           image = "goharbor/harbor-cli:latest"
-
-#           command = ["/bin/sh", "-c", "sleep infinity"]
-
-#           env_from {
-#             secret_ref {
-#               name = kubernetes_secret.harbor_credentials.metadata[0].name
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.harbor_credentials.metadata[0].name
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
 
 # Terraform Variables for GitHub Secrets
