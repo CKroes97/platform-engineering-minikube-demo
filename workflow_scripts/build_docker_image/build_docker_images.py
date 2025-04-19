6b1c20ec5959fe1ed068f3e978f03d7b401cabc9
Casper Kroes <c.kroes@watchmen.io>
2025-04-19 15:07:46 +0200

    fix path

--- workflow_scripts/build_docker_image/build_docker_images.py
+++ workflow_scripts/build_docker_image/build_docker_images.py
@@ -24,7 +24,7 @@ def parse_arguments():
 def find_dockerfiles(dockerfiles_dir):
     dockerfiles_dir = Path(dockerfiles_dir)
     dockerfile_paths = [
-        str(path / "Dockerfile")
+        str(path)
         for path in dockerfiles_dir.iterdir()
         if path.is_dir() and (path / "Dockerfile").exists()
     ]
@@ -59,8 +59,12 @@ def build_and_push_image(client, dockerfile_path, docker_registry):
         print(f"API error for {dockerfile_path}: {e}")
 
 
-def build_and_push_images(dockerfiles_dir, docker_registry):
-    dockerfiles_dir = Path(dockerfiles_dir)
+
+def main():
+    args = parse_arguments()
+    dockerfiles_dir_name = args.dockerfiles_dir
+    docker_registry = args.docker_registry
+    dockerfiles_dir = Path(dockerfiles_dir_name)
     if not dockerfiles_dir.exists():
         print(f"Directory '{dockerfiles_dir}' does not exist.")
         return
@@ -79,11 +83,6 @@ def build_and_push_images(dockerfiles_dir, docker_registry):
             build_and_push_image(client, dockerfile_path, docker_registry)
     except Exception as e:
         print(f"An error occurred: {e}")
-
-
-def main():
-    args = parse_arguments()
-    build_and_push_images(args.dockerfiles_dir, args.docker_registry)
 
 
 if __name__ == "__main__":
