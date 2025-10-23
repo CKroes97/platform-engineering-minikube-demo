c0575e560bad41fb975aa7ed0c8929ae7aa4f861
Casperk97 <casperkroes@gmail.com>
2025-10-23 18:57:11 +0200

    set to root

--- container-init.sh
+++ container-init.sh
@@ -8,22 +8,12 @@ usage() {
 
 # Prompt user for inputs
 read -p "Enter GitHub Runner Token: " GITHUB_RUNNER_TOKEN
-read -s -p "Enter password for $RUNNER_USER: " RUNNER_PASS
 
 # Validate inputs
-if [[ -z "$GITHUB_RUNNER_TOKEN" || -z "$RUNNER_USER" || -z "$RUNNER_PASS" ]]; then
+if [[ -z "$GITHUB_RUNNER_TOKEN"]]; then
     echo "Error: All inputs are required."
     exit 1
 fi
 
-$RUNNER_USER="runner"
-
-# Grant passwordless sudo access
-echo "$RUNNER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$RUNNER_USER
-
-# Change ownership & permissions
-sudo chown "$RUNNER_USER:$RUNNER_USER" "setup_runner.sh"
-sudo chmod +x "setup_runner.sh"
-
 # Execute the script as the new user
-sudo runuser -p "$RUNNER_USER" -c "bash $(dirname $0)/setup_runner.sh '$GITHUB_RUNNER_TOKEN' '$RUNNER_USER'"
+sudo $(dirname $0)/setup_runner.sh '$GITHUB_RUNNER_TOKEN' '$RUNNER_USER'
